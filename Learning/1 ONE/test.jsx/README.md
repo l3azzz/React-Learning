b3474bef47e93b02c2efc05b4b14050028c2e0c6
l3azzz <homecph1976@gmail.com>
2025-08-13 18:42:40 +0530

    so

--- Learning/1 ONE/test.jsx/README.md
+++ Learning/1 ONE/test.jsx/README.md
@@ -1,3 +1,7 @@
+<!-- edited and learned -->
+
+app.js
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- Learning/1 ONE/test.jsx/src/App.js
+++ Learning/1 ONE/test.jsx/src/App.js
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+//   return <h1>Hello World</h1>; // but now we can call this as html
+//   // some amount of data return ed by a function is called functional components
+// }
+// // we call this bcuz there are class based function
 
-function App() {
-  return (
-    <div className="App">
+import React from "react";
+class App extends React.Component {
+  render() {
+    // render function is used to render thing it ihas the responsibilty of that
+    <h1>Hello World</h1>;
+  }
+}
+
+export default App;
+// above code is the full code of class based components
+
+// helllo world program
+// first open app.js
+// app.js main parent file
+
+// it looks like html
+// but js like "class"
+// athukond nammuk ozhivaakkaam
+
+/*
+import logo from "./logo.svg";
+import "./App.css";
+
+
+
+
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +46,6 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+    
+    
+    */
