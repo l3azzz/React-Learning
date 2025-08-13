d31d8ffb5ebacf2531d26be8cda2e1f2193d744e
l3azzz <homecph1976@gmail.com>
2025-08-13 18:03:02 +0530

    .

---
+++ Learning/TO 10/1 ONE/one4.jsx
@@ -0,0 +1,11 @@
+// INSTALLATION
+
+// FIRST WE WANNA INSTALL NODE.JS
+//      to check node is instlled type in cmd node -v
+
+// then we wanna create a react app
+//     to install run these command
+
+// npx create-react-app (name)
+// cd tutorial
+// npm start
