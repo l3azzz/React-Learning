35d5d1f7fc70a98b56600ffedfcc1a4b80c8cbf9
l3azzz <homecph1976@gmail.com>
2025-08-17 12:05:49 +0530

    ea

--- Learning/first.app/src/App15.js
+++ Learning/first.app/src/App15.js
@@ -1,16 +1,30 @@
+// adapting based on propss
+
 import React from "react";
+import styled from "styled-components";
 class App extends React.Component {
   render() {
     return (
       <>
         <ul>
           <li>
-            this is the first app15.js <hr />
+            {/* <Heading color="violet"> */}
+            this is the first app15.js suiiiasfaf
+            {/* </Heading> */}
+            <hr />
           </li>
         </ul>
       </>
     );
   }
 }
+// const Heading = styled.h1`
+//   font-size: 40px;
+//   color: ${({ color }) => color};
+//   &:hover {
+//     color: blue;
+//   }
+// `;
+// custom properties that can be modified
 
 export default App;
